a31652be8e17af5abd8372bae84c1e6b2e0d2203
Karishma Bhardwaj <karishmabhardwaj12345@gmail.com>
2025-10-31 17:09:02 +0530

    commit

---
+++ worksphere/src/main/java/com/karishma/worksphere/controller/BoardRequestController.java
@@ -0,0 +1,11 @@
+package com.karishma.worksphere.controller;
+
+import com.karishma.worksphere.model.dto.request.BoardRequestDTO;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class BoardRequestController {
+    public BoardRequestResponse createBoardRequest(@RequestBody BoardRequestDTO request)
+    {
+
+    }
+}
---
+++ worksphere/src/main/java/com/karishma/worksphere/model/dto/request/BoardRequestDTO.java
@@ -0,0 +1,8 @@
+package com.karishma.worksphere.model.dto.request;
+
+public class BoardRequestDTO {
+    private String project_name;
+    private String project_key;
+    private String description;
+    private String justification;
+}
