439eb3eaf9c78bac5668c6eb9667a582af7fd2fc
Karishma Bhardwaj <karishmabhardwaj12345@gmail.com>
2025-11-01 13:48:43 +0530

    ProjectRequest entity creation

--- worksphere/src/main/java/com/karishma/worksphere/model/entity/BoardRequest.java
+++ worksphere/src/main/java/com/karishma/worksphere/model/entity/BoardRequest.java
@@ -2,13 +2,16 @@ package com.karishma.worksphere.model.entity;
 
 import com.karishma.worksphere.model.enums.Status;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 @Entity
@@ -18,23 +21,26 @@ public class BoardRequest {
     private UUID board_request_id;
     @Column(nullable = false,unique = true)
     private String project_name;
-    @Column(nullable = false)
+    @Column(nullable = false,unique=true)
     private String project_key;
     @Column(nullable = false)
     private String description;
 
     private String justification;
     @Enumerated(EnumType.STRING)
-    private Status status=Status.PENDING
-            ;
+    @Builder.Default
+    @Column(nullable = false)
+    private Status status=Status.PENDING;
     @CreationTimestamp
+    @Column(nullable = false,updatable = false)
     private LocalDateTime requestedAt;
     @OneToOne
-    @JoinColumn(name="requester_id")
+    @JoinColumn(name="requester_id",nullable = false)
     private User requester;
     @OneToOne
     @JoinColumn(name="reviewer_id")
     private User reviewedBy;
     private String rejection_reason;
+    
 
 }
