b18b6c0360dfcce04a2e490bee234841a6f88c7c
Karishma Bhardwaj <karishmabhardwaj12345@gmail.com>
2025-11-09 02:22:00 +0530

    WorkflowTransition entity created

---
+++ worksphere/src/main/java/com/karishma/worksphere/model/entity/WorkflowTransition.java
@@ -0,0 +1,36 @@
+package com.karishma.worksphere.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WorkflowTransition {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private UUID work_transition_id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="workflow_id",nullable = false)
+    Workflow workflow;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="from_status")
+    private WorkflowStatus fromStatus;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="to_status")
+    private WorkflowStatus toStatus;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "role")
+    private Set<String> allowedRoles;
+
+
+
+}
