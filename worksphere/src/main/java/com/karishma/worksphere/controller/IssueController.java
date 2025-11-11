c1f3d91bc2626e8df8e9446acdb9ba966aab6de6
Karishma Bhardwaj <karishmabhardwaj12345@gmail.com>
2025-11-11 11:25:23 +0530

    CreateIssueDTO created

--- worksphere/src/main/java/com/karishma/worksphere/controller/IssueController.java
+++ worksphere/src/main/java/com/karishma/worksphere/controller/IssueController.java
@@ -1,10 +1,18 @@
 package com.karishma.worksphere.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.karishma.worksphere.model.dto.response.IssueResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestBody("/api/boards")
+@RequestMapping("/api/boards")
 public class IssueController {
+
+    @PostMapping("/{board_id}/create-issue")
+    public IssueResponse createIssue(@PathVariable UUID board_id, @RequestBody CreateIssueDTO request)
+    {
+
+    }
 
 }
---
+++ worksphere/src/main/java/com/karishma/worksphere/model/dto/request/CreateIssueDTO.java
@@ -0,0 +1,39 @@
+package com.karishma.worksphere.model.dto.request;
+
+import com.karishma.worksphere.model.enums.IssueType;
+import com.karishma.worksphere.model.enums.Priority;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateIssueDTO
+{
+
+        @NotNull(message="Issue type is required")
+        private IssueType issue_type;
+        @NotBlank(message="Summary is required")
+        private String summary;
+        @NotNull(message="Priority is required")
+        private Priority priority;
+        private UUID assignee_id;
+        private String description;
+        private UUID parent_id;
+        private UUID epic_id;
+        private LocalDate due_Date;
+
+
+}
+
+
+
