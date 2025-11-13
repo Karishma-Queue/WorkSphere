a9cb228dad16a7f5e581eab8c5f111c503dca9cf
Karishma Bhardwaj <karishmabhardwaj12345@gmail.com>
2025-11-13 14:52:39 +0530

    Changes made in issue repository

---
+++ worksphere/src/main/java/com/karishma/worksphere/model/dto/request/UpdateIssueDTO.java
@@ -0,0 +1,22 @@
+package com.karishma.worksphere.model.dto.request;
+
+import com.karishma.worksphere.model.enums.Priority;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateIssueDTO {
+    private String summary;
+    private String description;
+    private Priority priority;
+    private UUID assignee_id;
+    private LocalDate due_date;
+}
--- worksphere/src/main/java/com/karishma/worksphere/repository/IssueRepository.java
+++ worksphere/src/main/java/com/karishma/worksphere/repository/IssueRepository.java
@@ -3,7 +3,11 @@ package com.karishma.worksphere.repository;
 import com.karishma.worksphere.model.entity.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IssueRepository extends JpaRepository<Issue, UUID> {
+    List<Issue> findByBoard_BoardId(UUID boardId);
+
+
 }
