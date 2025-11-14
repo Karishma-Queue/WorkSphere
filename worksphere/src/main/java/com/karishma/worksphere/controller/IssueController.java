cd461747267f89cdace24f9c5e31d69b33eb810d
Karishma Bhardwaj <karishmabhardwaj12345@gmail.com>
2025-11-14 14:00:57 +0530

    issuecontroller

--- worksphere/src/main/java/com/karishma/worksphere/controller/IssueController.java
+++ worksphere/src/main/java/com/karishma/worksphere/controller/IssueController.java
@@ -3,6 +3,9 @@ package com.karishma.worksphere.controller;
 import com.karishma.worksphere.model.dto.request.CreateIssueDTO;
 import com.karishma.worksphere.model.dto.request.UpdateIssueDTO;
 import com.karishma.worksphere.model.dto.response.IssueResponse;
+import com.karishma.worksphere.security.annotation.AllowOnlyProjAdmin;
+import com.karishma.worksphere.security.annotation.BoardIdParam;
+import com.karishma.worksphere.security.annotation.IssueIdParam;
 import com.karishma.worksphere.service.IssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,41 +21,47 @@ public class IssueController {
 
     private final IssueService issueService;
 
+    @AllowOnlyProjAdmin
     @PostMapping
     public ResponseEntity<IssueResponse> createIssue(
-            @PathVariable UUID boardId,
+            @PathVariable @BoardIdParam UUID boardId,
             @RequestBody CreateIssueDTO request) {
         IssueResponse response = issueService.createIssue(boardId, request);
         return ResponseEntity.ok(response);
     }
 
+    @AllowOnlyProjAdmin
     @GetMapping
-    public ResponseEntity<List<IssueResponse>> getAllIssues(@PathVariable UUID boardId) {
+    public ResponseEntity<List<IssueResponse>> getAllIssues(
+            @PathVariable @BoardIdParam UUID boardId) {
         List<IssueResponse> responses = issueService.getAllIssues(boardId);
         return ResponseEntity.ok(responses);
     }
 
+    @AllowOnlyProjAdmin
     @GetMapping("/{issueId}")
     public ResponseEntity<IssueResponse> getIssueById(
             @PathVariable UUID boardId,
-            @PathVariable UUID issueId) {
+            @PathVariable @IssueIdParam UUID issueId) {
         IssueResponse response = issueService.getIssueById(boardId, issueId);
         return ResponseEntity.ok(response);
     }
 
+    @AllowOnlyProjAdmin
     @PutMapping("/{issueId}")
     public ResponseEntity<IssueResponse> updateIssue(
             @PathVariable UUID boardId,
-            @PathVariable UUID issueId,
+            @PathVariable @IssueIdParam UUID issueId,
             @RequestBody UpdateIssueDTO request) {
         IssueResponse response = issueService.updateIssue(boardId, issueId, request);
         return ResponseEntity.ok(response);
     }
 
+    @AllowOnlyProjAdmin
     @DeleteMapping("/{issueId}")
     public ResponseEntity<Void> deleteIssue(
             @PathVariable UUID boardId,
-            @PathVariable UUID issueId) {
+            @PathVariable @IssueIdParam UUID issueId) {
         issueService.deleteIssue(boardId, issueId);
         return ResponseEntity.noContent().build();
     }
@@ -60,7 +69,7 @@ public class IssueController {
     @PutMapping("/{issueId}/status/{statusId}")
     public ResponseEntity<IssueResponse> changeStatus(
             @PathVariable UUID boardId,
-            @PathVariable UUID issueId,
+            @PathVariable @IssueIdParam UUID issueId,
             @PathVariable UUID statusId) {
         IssueResponse response = issueService.changeIssueStatus(boardId, issueId, statusId);
         return ResponseEntity.ok(response);
--- worksphere/src/main/java/com/karishma/worksphere/controller/WorkflowController.java
+++ worksphere/src/main/java/com/karishma/worksphere/controller/WorkflowController.java
@@ -7,6 +7,7 @@ import com.karishma.worksphere.model.dto.request.WorkflowUpdateDTO;
 import com.karishma.worksphere.model.dto.response.*;
 import com.karishma.worksphere.security.annotation.AllowOnlyProjAdmin;
 import com.karishma.worksphere.security.annotation.BoardIdParam;
+import com.karishma.worksphere.security.annotation.WorkflowIdParam;
 import com.karishma.worksphere.service.WorkflowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -39,38 +40,42 @@ public class WorkflowController {
     //
     @AllowOnlyProjAdmin
     @PutMapping("/{workflow_id}")
-    public ResponseEntity<WorkflowUpdateResponse> updateWorkflow(@PathVariable UUID workflow_id,
+    public ResponseEntity<WorkflowUpdateResponse> updateWorkflow(@PathVariable @WorkflowIdParam UUID workflow_id,
                                                            @RequestBody WorkflowUpdateDTO request)
 
     {
         WorkflowUpdateResponse response= workflowService.updateWorkflow(workflow_id,request);
              return ResponseEntity.ok(response);
     }
+    @AllowOnlyProjAdmin
    @DeleteMapping("/{workflow_id}")
-    public ResponseEntity<?> deleteWorkflow(@PathVariable UUID workflow_id)
+    public ResponseEntity<?> deleteWorkflow(@PathVariable @WorkflowIdParam UUID workflow_id)
    {
        workflowService.deleteWorkflow(workflow_id);
        return ResponseEntity.ok("Deleted successfully");
    }
+   @AllowOnlyProjAdmin
    @PostMapping("/{workflow_id}/statuses")
-    public StatusResponse addStatus(@PathVariable UUID workflow_id, @RequestBody AddStatusDTO request)
+    public StatusResponse addStatus(@PathVariable @WorkflowIdParam UUID workflow_id, @RequestBody AddStatusDTO request)
    {
        return workflowService.addStatus(workflow_id,request);
    }
+   @AllowOnlyProjAdmin
    @DeleteMapping("/{workflow_id}/statuses/{status_id}")
-    public ResponseEntity<String> deleteStatus(@PathVariable UUID workflow_id,@PathVariable UUID status_id)
+    public ResponseEntity<String> deleteStatus(@PathVariable @WorkflowIdParam  UUID workflow_id,@PathVariable UUID status_id)
    {
        workflowService.deleteStatus(workflow_id,status_id);
        return ResponseEntity.ok("Deleted successfully status with id "+status_id);
    }
+   @AllowOnlyProjAdmin
    @PostMapping("/{workflow_id}/transitions")
-    public TransitionResponse addTransition(@PathVariable UUID workflow_id,@RequestBody TransitionRequest request)
-
+    public TransitionResponse addTransition(@PathVariable  @WorkflowIdParam  UUID workflow_id,@RequestBody TransitionRequest request)
    {
        return workflowService.addTransition(workflow_id,request);
    }
+   @AllowOnlyProjAdmin
    @DeleteMapping("/{workflow_id}/transitions/{transition_id}")
-    public ResponseEntity<String> removeTransition(@PathVariable UUID workflow_id,@PathVariable UUID transition_id)
+    public ResponseEntity<String> removeTransition(@PathVariable @WorkflowIdParam UUID workflow_id,@PathVariable UUID transition_id)
    {
        workflowService.deleteTransition(workflow_id,transition_id);
        return ResponseEntity.ok("Deleted successfully");
