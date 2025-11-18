95438424605b7dcfe46d2d92d6cb2a24296f9656
Karishma Bhardwaj <karishmabhardwaj12345@gmail.com>
2025-11-18 16:17:43 +0530

    Sprint entity created

---
+++ worksphere/src/main/java/com/workify/worksphere/model/entity/Sprint.java
@@ -0,0 +1,49 @@
+package com.workify.worksphere.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sprint {
+  @EmbeddedId
+  private SprintId sprintId;
+  private SprintName sprintName;
+  @Column(nullable = false)
+  private LocalDate startDate;
+  @Column(nullable=false)
+  private LocalDate endDate;
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private SprintStatus status;
+  @ManyToOne
+  @JoinColumn(name = "board_id", nullable = false)
+  private Board board;
+  @ManyToOne
+  @JoinColumn(name="created_by",nullable=false)
+  private User createdBy;
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  private LocalDateTime startedAt;
+
+  private LocalDateTime completedAt;
+
+
+}
